ed0d6f154bb37af829f480ec5d3f5d50cd1c8733
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2023-02-07 10:23:35 +0100

    Work with Redis

--- service/services/models.py
+++ service/services/models.py
@@ -1,11 +1,14 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from django.db.models.signals import post_delete
 
 from clients.models import Client
+from .receivers import delete_cache_total_sum
 from .tasks import set_price, set_comment
 
 
 class Service(models.Model):
+    """Service model"""
     name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
 
@@ -28,6 +31,8 @@ class Service(models.Model):
 
 
 class Plan(models.Model):
+    """Service tariff plan model"""
+
     class PlanTypes(models.TextChoices):
         full = 'Full'
         student = 'Student'
@@ -57,11 +62,22 @@ class Plan(models.Model):
 
 
 class Subscription(models.Model):
+    """Service subscription model"""
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
     comment = models.CharField(max_length=100, blank=True)
 
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result = super().save(*args, **kwargs)
+        if creating:
+            set_price.delay(self.id)
+        return result
+
     def __str__(self):
         return f'{self.client} | Service: {self.service} | {self.plan}'
+
+
+post_delete.connect(delete_cache_total_sum, sender=Subscription)
--- service/services/tasks.py
+++ service/services/tasks.py
@@ -3,12 +3,19 @@ import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.conf import settings
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import F
 
 
 @shared_task(base=Singleton)
 def set_price(subscription_id):
+    """
+    Celery task which recalculates the subscription
+    cost based on the discount percentage and
+    the cost of the tariff plan
+    """
     from services.models import Subscription
 
     with transaction.atomic():
@@ -18,10 +25,15 @@ def set_price(subscription_id):
 
         subscription.price = subscription.anotated_price
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 
 @shared_task(base=Singleton)
 def set_comment(subscription_id):
+    """
+    Celery task which set current time of
+    updating/creating in subscription as 'Comment'
+    """
     from services.models import Subscription
 
     with transaction.atomic():
@@ -29,3 +41,4 @@ def set_comment(subscription_id):
 
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
